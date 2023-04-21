5f6996c7e1652bab7bdb7b4e24a4255a55bfb012
omarTaipicana <fernandoparadoja@gmail.com>
2023-04-20 23:31:50 -0500

    HEADER

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import ENCABEZADO from "../assets/images/ENCABEZADO.jpg"
+import WA from "../assets/images/WA.png"
+import FB from "../assets/images/FB.png"
 
-import { useNavigate } from "react-router-dom";
+
+import { Link, useNavigate } from "react-router-dom";
 
 const MyNav = () => {
-    const navigate = useNavigate()
-    return (
-        <div>
-           
+  const navigate = useNavigate()
+  return (
+    <div>
+
       <header>
         <div className='header'>
-          <img className='img-header' src={ENCABEZADO} alt="Escudo Policía Nacional del Ecuador" />
-         
+          <img className='img-header' src={ENCABEZADO} alt="ENCABEZADO" />
+          <div className='header-icon'>
+            <a href="https://api.whatsapp.com/send?phone=593988202320" target="_blank" > <img className='img-header-icon-wa' src={WA} alt="WA" /></a>
+            <a href="https://www.facebook.com/mandaditosxpresspuyo/?mibextid=ZbWKwL" target="_blank" > <img className='img-header-icon-fb' src={FB} alt="FB" /></a>
+          </div>
         </div>
         <nav>
           <ul className='ul-header'>
@@ -21,8 +27,8 @@ const MyNav = () => {
           </ul>
         </nav>
       </header>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default MyNav;
