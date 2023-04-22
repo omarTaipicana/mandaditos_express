b603df24b0417b5fffb241e26c287f3e75d1ef8a
omarTaipicana <fernandoparadoja@gmail.com>
2023-04-22 01:09:34 -0500

    CARRUCEL

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Slider from "../components/Slider"
 
 const Home = () => {
     return (
         <div>
-            ESTE ES EL HOME
+            <div className='home'>
+                <div className='video-home-content'>
+                <iframe src="https://drive.google.com/file/d/1-AYOebq_JutD8RzHCPoPKKg6NuBzlHP5/preview" width="100%" height="100%" allow="autoplay"></iframe>
+                </div>
+                <Slider ></Slider>
+            </div>
         </div>
     );
 };
